01866fbf721f660e4c1ea0960af0ca31588739ba
mehditounsi <mehditounsi1997@gmail.com>
2021-03-29 19:48:29 +0100

    capstone deployment

--- service/server/index.js
+++ service/server/index.js
@@ -7,11 +7,11 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-
+app.use('/app.js',express.static(__dirname + '/../public/bundle.js'));
  app.use(express.static(__dirname + '/../public'));
 
  app.use('',routes)
 
- app.listen(3000, () => {
-    console.log(`listening on port 3000`);
-  });
+ app.listen(3001, () => {
+    console.log(`listening on port 3001`);
+  })
